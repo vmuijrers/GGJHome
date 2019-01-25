193df8176d2b849421c42996aba43dababa80ba1
MikouZonata <kevin.vanas@student.hku.nl>
2019-01-25 22:22:46 +0100

    CrabMove

--- Home/Assets/Scripts/CrabMove.cs
+++ Home/Assets/Scripts/CrabMove.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class CrabMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public int playerIndex = 0;
+	GamePadState gamePadState;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	Vector2 leftStickInput;
+
+	Rigidbody rig;
+
+	[Header("Settings")]
+	public float speed = 5;
+
+	private void Awake ()
+	{
+		rig = GetComponent<Rigidbody>();
+	}
+
+	private void Update ()
+	{
+		gamePadState = GamePad.GetState((PlayerIndex) playerIndex);
+		if (gamePadState.IsConnected) {
+			GetInputs();
+			Debug.Log(leftStickInput + " " + playerIndex);
+			
+			Move(leftStickInput);
+		}
+	}
+
+	void GetInputs ()
+	{
+		leftStickInput = new Vector2(gamePadState.ThumbSticks.Left.X, gamePadState.ThumbSticks.Left.Y);
+	}
+
+	void Move (Vector2 direction)
+	{
+		direction = direction.normalized;
+		rig.velocity = new Vector3(direction.x * speed, 0, direction.y * speed);
+	}
 }
