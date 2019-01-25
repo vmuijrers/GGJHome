80e5777ccbebf838cc3efe20be4a4122ddb501bc
Valentijn Muijrers <vmuijrers@gmail.com>
2019-01-25 22:28:14 +0100

    Added Messages

--- Home/Assets/Scripts/CameraController.cs
+++ Home/Assets/Scripts/CameraController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Transform targetToFollow;
+    private float zoomDistance;
+    private float zoomIncrease = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnEnable() {
+        Message.AddListener(MessageEnum.ON_GRAB_SHELL,gameObject, Shake);
+
+
+    }
+
+    private void OnDisable() {
+        Message.RemoveListenener(MessageEnum.ON_GRAB_SHELL, gameObject, Shake);
+    }
+
+    private Shake() {
+
     }
 }
