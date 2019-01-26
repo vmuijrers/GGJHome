32570a131459cc58e55b124a8fbf79e6d7e71af8
Valentijn Muijrers <vmuijrers@gmail.com>
2019-01-26 16:59:36 +0100

    Shell updated

---
+++ Home/Assets/Scripts/CliffSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CliffSpawner : MonoBehaviour
+{
+    public GameObject cliffPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0;i< 20; i++) {
+            Instantiate(cliffPrefab, new Vector3(i * 9.6f, 0, -15.6f),Quaternion.Euler(0,180,0));
+            Instantiate(cliffPrefab, new Vector3(i * 9.6f, 0, 15.6f),Quaternion.identity);
+        }
+    }
+
+}
--- Home/Assets/Scripts/ShellEntrance.cs
+++ Home/Assets/Scripts/ShellEntrance.cs
@@ -5,22 +5,24 @@ using XInputDotNetPure;
 
 public class ShellEntrance : MonoBehaviour
 {
-    public Crab attachedCrab;
+    public List<Crab> attachedCrabs = new List<Crab>();
     public Shell shell;
-
-    public bool isCrabAttached() {
-        return attachedCrab != null;
-    }
 
     public void Init(Shell shell) {
         this.shell = shell;
     }
 
     public void AttachCrab(Crab crab) {
-        attachedCrab = crab;
+        if (!attachedCrabs.Contains(crab)) {
+            attachedCrabs.Add(crab);
+            shell.UpdateCrabVisuals(attachedCrabs.Count);
+        }
     }
 
     public void DetachCrab(Crab crab) {
-        attachedCrab = null;
+        if (attachedCrabs.Contains(crab)) {
+            attachedCrabs.Remove(crab);
+            shell.UpdateCrabVisuals(attachedCrabs.Count);
+        }
     }
 }
