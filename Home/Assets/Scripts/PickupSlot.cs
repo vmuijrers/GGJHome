88ca94a256c53dbdedf9882d4122d1b8a7476b0c
Valentijn Muijrers <vmuijrers@gmail.com>
2019-01-26 18:42:23 +0100

    PickupSlot + Music

---
+++ Home/Assets/Scripts/PickupSlot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSlot : MonoBehaviour
+{
+    [HideInInspector] public GameObject reference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
