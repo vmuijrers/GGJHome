a487510419db93eb7235ec1346bcea3d28fe358b
MikouZonata <kevin.vanas@student.hku.nl>
2019-01-27 13:06:54 +0100

    Fades

--- Home/Assets/Scripts/LevelManager.cs
+++ Home/Assets/Scripts/LevelManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using XInputDotNetPure;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -10,11 +13,37 @@ public class LevelManager : MonoBehaviour
 
 	bool gameRunning = false;
 
+	Crab[] crabs;
+
+	public Image fadeImage;
+	public float fadeOutTime = 1.5f;
+
     void Start()
     {
+		StartCoroutine(FadeIn());
         pickupPrefab = Resources.Load<Pickup>("Pickups/Pickup");
         SpawnLevel();
+		crabs = FindObjectsOfType<Crab>();
     }
+
+	private void Update ()
+	{
+		bool oneAlive = false;
+		foreach (Crab c in crabs) {
+			if (!c.dood)
+				oneAlive = true;
+		}
+		if (!oneAlive) {
+			StartCoroutine(FadeOut());
+		}
+
+		for (int i = 0; i < 2; i++) {
+			GamePadState state = GamePad.GetState((PlayerIndex) i);
+			if (state.Buttons.Back == ButtonState.Pressed) {
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
 
 	void SpawnLevel() {
         for(int i=0; i < amountOfPickups; i++) {
@@ -22,4 +51,24 @@ public class LevelManager : MonoBehaviour
             pickup.Init(Util.Choose(DecorationType.Basic, DecorationType.Star));
         }
     }
+
+	IEnumerator FadeOut ()
+	{
+		for (float t = 0; t < 1; t += Time.deltaTime / fadeOutTime) {
+			Color c = new Color(0, 0, 0, t);
+			fadeImage.color = c;
+			yield return null;
+		}
+
+		SceneManager.LoadScene(0);
+	}
+
+	IEnumerator FadeIn ()
+	{
+		for (float t = 1; t > 0; t -= Time.deltaTime / fadeOutTime) {
+			Color c = new Color(0, 0, 0, t);
+			fadeImage.color = c;
+			yield return null;
+		}
+	}
 }
