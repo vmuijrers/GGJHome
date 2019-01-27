9006f33e6444e3363e1852f462461de8b7eb3db3
MikouZonata <kevin.vanas@student.hku.nl>
2019-01-27 11:26:29 +0100

    Vegetation Spawner

--- Home/Assets/Scripts/VegetationSpawner.cs
+++ Home/Assets/Scripts/VegetationSpawner.cs
@@ -7,7 +7,7 @@ public class VegetationSpawner : MonoBehaviour
 	public GameObject[] vegetationPrefabs;
 
 	Vector3 lastSpawnPos = Vector3.zero, camPos;
-	public float maxSpawnZPos = 13, spawnXOffset = 17.5f, distanceBetweenSpawns = 4;
+	public float maxSpawnZPos = 13, spawnXOffset = 17.5f, distanceBetweenSpawns = 1;
 
 	private void Awake ()
 	{
@@ -19,7 +19,7 @@ public class VegetationSpawner : MonoBehaviour
 		while (true) {
 			camPos = new Vector3(Camera.main.transform.position.x, 0, Camera.main.transform.position.z);
 			if (Util.SquareDistance(camPos, lastSpawnPos) > distanceBetweenSpawns * distanceBetweenSpawns) {
-				Instantiate(Util.Choose(vegetationPrefabs), new Vector3(camPos.x + spawnXOffset + Random.Range(0, 3.0f), 0, Random.Range(-maxSpawnZPos, maxSpawnZPos)), Quaternion.Euler(0, Random.Range(0, 360), 0));
+				Instantiate(Util.Choose(vegetationPrefabs), new Vector3(camPos.x + spawnXOffset + Random.Range(0, 1.0f), 0, Random.Range(-maxSpawnZPos, maxSpawnZPos)), Quaternion.identity);
 				lastSpawnPos = camPos;
 			}
 			yield return null;
